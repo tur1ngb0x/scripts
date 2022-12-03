9dfd3ba09b210498422d805955399fd8625e7c74
tur1ngb0x <25666761+tur1ngb0x@users.noreply.github.com>
2022-12-03 18:04:27 +0530

    2022-12-03 Sat 18:04:27 IST

--- install-novideo.sh
+++ install-novideo.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+# blacklist nvidia modules
 mkdir -pv /etc/modprobe.d
 cat << EOF | sudo tee /etc/modprobe.d/novideo.conf
 alias nouveau off
@@ -14,6 +15,7 @@ blacklist nvidia_modeset
 blacklist nvidia_uvm
 EOF
 
+# remove nvidia devices
 mkdir -pv /etc/udev/rules.d
 cat << EOF | sudo tee /etc/udev/rules.d/novideo.rules
 ACTION=="add", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{class}=="0x03[0-9]*", ATTR{power/control}="auto", ATTR{remove}="1"
@@ -22,23 +24,28 @@ ACTION=="add", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{class}=="0x0c0330"
 ACTION=="add", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{class}=="0x0c8000", ATTR{power/control}="auto", ATTR{remove}="1"
 EOF
 
+# print info
 cat << EOF
-# Remove intel driver
+# Remove intel driver (optional)
 $ sudo apt-get purge xserver-xorg-video-intel
 $ sudo pacman -Rns xf86-video-intel
 $ sudo rm -fv /etc/X11/xorg.conf.d/20-intel.conf
 
-# Remove nvidia driver
+# Remove nvidia driver (optional)
 $ sudo apt-get purge ~nnvidia
+$ sudo pacman -Rns nvidia nvidia-lts
+$ sudo dnf remove akmod-nvidia* kmod-nvidia*
+$ sudo rm -fv /etc/X11/xorg.conf.d/20-nvidia.conf
+$ sudo rm -fv /usr/share/X11/xorg.conf.d/10-nvidia-drm-outputclass.conf
 
-# Set nvidia prime
+# Set nvidia prime (mandatory)
 $ sudo prime-select on-demand
 
-# Disable gpu service
+# Disable gpu service (mandatory)
 $ systemctl disable gpu-manager.service
 
-# Update initramfs
-$ sudo update-initramfs -u -k all -v
-$ sudo dracut --force --regenerate-all
-$ sudo mkinitcpio -Pv
+# Update initramfs (mandatory)
+$ sudo update-initramfs -u -k all -v		# debian/ubuntu
+$ sudo dracut --force --regenerate-all		# fedora/rhel
+$ sudo mkinitcpio -Pv						# arch/manjaro
 EOF
