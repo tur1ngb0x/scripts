d182ba6a40bb9968fc17c663b9ac968922601900
tur1ngb0x <25666761+tur1ngb0x@users.noreply.github.com>
2023-01-05 11:41:35 +0530

    update

--- wip/screenclip.sh
+++ wip/screenclip.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
 
+# check for gnome-screenshot
+[[ ! -f /usr/bin/gnome-screenshot ]] && notify-send 'gnome-screenshot is not installed' && exit
+
+# check for screenshot directory
 [[ ! -d "${HOME}/Pictures/Screenshots" ]] && mkdir -p "${HOME}/Pictures/Screenshots"
 
+# take area screenshot, copy to clipboard and save to file
 gnome-screenshot --area --clipboard --file="${HOME}/Pictures/Screenshots/screenclip_$(date +'%Y%m%d-%H%M%S').png"
+
