c1f4dead45848239080c5733610bd4dae9ef3e62
tur1ngb0x <25666761+tur1ngb0x@users.noreply.github.com>
2024-04-24 12:11:43 +0530

    Update enable-wsl.cmd

--- windows/enable-wsl.cmd
+++ windows/enable-wsl.cmd
@@ -1,14 +1,19 @@
 @echo on
 
+echo ------------------------------------------------------------------------
 echo Microsoft-Windows-Subsystem-Linux will be ENABLED.
 echo VirtualMachinePlatform will be ENABLED.
+echo Oracle Virtualbox will be DISABLED.
+echo ------------------------------------------------------------------------
 
 pause
 
 dism.exe /online /enable-feature /featurename:Microsoft-Windows-Subsystem-Linux /all /norestart
 dism.exe /online /enable-feature /featurename:VirtualMachinePlatform /all /norestart
 
+echo ------------------------------------------------------------------------
 echo Machine will be restarted now, close window to skip restart
+echo ------------------------------------------------------------------------
 
 pause
 shutdown.exe /f /t 0 /r
