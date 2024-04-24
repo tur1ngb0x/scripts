5de2eaf7abec5abb770b29790020d28b17f6875a
tur1ngb0x <25666761+tur1ngb0x@users.noreply.github.com>
2024-04-24 13:51:11 +0530

    Update enable-vbox.cmd

--- windows/enable-vbox.cmd
+++ windows/enable-vbox.cmd
@@ -1,14 +1,22 @@
 @echo off
 
+title ENABLE VBOX
+
+echo ------------------------------------------------------------------------
+echo Oracle Virtualbox will be ENABLED.
 echo Microsoft-Windows-Subsystem-Linux will be DISABLED.
 echo VirtualMachinePlatform will be DISABLED.
+echo ------------------------------------------------------------------------
 
 pause
 
-dism.exe /online /disable-feature /featurename:Microsoft-Windows-Subsystem-Linux /norestart
-dism.exe /online /disable-feature /featurename:VirtualMachinePlatform /norestart
+dism.exe /online /disable-feature /featurename:Microsoft-Windows-Subsystem-Linux /all /norestart
+dism.exe /online /disable-feature /featurename:VirtualMachinePlatform /all /norestart
 
-echo Machine will be restarted now, close window to skip restart
+echo ------------------------------------------------------------------------
+echo Machine will be RESTARTED now, close window to SKIP restart.
+echo ------------------------------------------------------------------------
 
 pause
+
 shutdown.exe /f /t 0 /r
