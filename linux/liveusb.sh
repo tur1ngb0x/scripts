bd3b7b506d719ba089a0f466b0b0a3d6f719bdd7
tur1ngb0x <25666761+tur1ngb0x@users.noreply.github.com>
2025-01-10 15:34:28 +0530

    Create liveusb.sh

---
+++ linux/liveusb.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+# time
+timedatectl set-timezone Asia/Kolkata
+timedatectl set-local-rtc 1
+timedatectl set-ntp 1
+timedatectl --adjust-system-clock
+
+# updates
+sudo bash -c 'apt-get clean && apt-get update && apt-get install curl git wget'
+
+# git
+mkdir -pv $HOME/src/
+git clone https://github.com/tur1ngb0x/dotfiles $HOME/src/
+git clone https://github.com/tur1ngb0x/scripts $HOME/src/
+
+# brave
+wget -4O '/tmp/brave.sh' 'https://dl.brave.com/install.sh
+sh '/tmp/brave.sh'
+
+# chrome
+wget -4O '/tmp/chrome.deb' 'https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb'
+sudo bash -c 'apt-get install /tmp/chrome.deb'
+
+# edge
+wget -4O '/tmp/edge.deb' 'https://go.microsoft.com/fwlink?linkid=2149051'
+sudo bash -c 'apt-get install /tmp/edge.deb'
